7d45d372514520b129dd1f8c4143fd423813f42e
dohaehab <dohaehab@gmail.com>
2024-11-18 16:34:59 +0200

    updated console

--- models/__init__.py
+++ models/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """create instance of file storage class"""
 from models.engine.file_storage import FileStorage
+from models.user import User
 
 storage = FileStorage()
 storage.reload()
